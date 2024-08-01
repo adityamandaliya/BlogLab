c9bfbc5b56461e937fec2ce4e208497a230ea4f5
adityamandaliya <adityamand@gmail.com>
2024-08-01 19:09:23 -0400

    Create identity project with UserStore.

--- BlogLab.Repository/IBlogRepository.cs
+++ BlogLab.Repository/IBlogRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlogLab.Models.Blog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace BlogLab.Repository
 {
-    internal interface IBlogRepository
+    public interface IBlogRepository
     {
+        public Task<Blog> UpsertAsync(BlogCreate blogCreate, int applicationUserId);
+
+        public Task<PageResults<Blog>> GetAllAsync(BlogPaging blogPaging);
+
+        public Task<Blog> GetAsync(int blogId);
+
+        public Task <List<Blog>> GetAllByUserIdAsync(int alpplicationuserId);
+
+        public Task<List<Blog>> GetAllFamousAsync();
+
+        public Task<int> DeleteAsync(int blogId);
     }
 }
